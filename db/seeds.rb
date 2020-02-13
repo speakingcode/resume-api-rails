a0edb0f6e54e3802eb8114a475992c82c3225b3d
speakingcode <dlissner@gmail.com>
2020-02-13 10:46:04 -0600

    Seed db and update schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+path = Rails.root.join('db')
+
+resume = JSON.parse(File.read path.join('seed.json'))
+
+Resume.create(resume)
